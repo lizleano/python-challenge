3323dddba2b6e886ce7b28ed282039671100dfbd
lizleano <lizleano35@gmail.com>
2017-08-22 23:45:06 -0400

    Upload resource and data folders, and comments to py_poll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -30,21 +30,23 @@ outpath = os.path.join('outpath', 'result_' + csvpath.rsplit('\\', 1)[1].rsplit(
 
 # Open the file using "write" mode. Specify the variable to hold the contents
 with open(outpath, 'w') as file:
+	# headers
 	file.write("Election Results\n")
 	file.write("-----------------------------------\n")
 	file.write("Total Votes: " + str(totalVotes) + '\n')
 	file.write("-----------------------------------\n")
-
 	print("Election Results")
 	print("-----------------------------------")
 	print("Total Votes: " + str(totalVotes))
 	print("-----------------------------------")
 
+	# candidate/percent won/votes won
 	for key, value in pollDict.items():
 		percent = round((value/totalVotes)*100, 1)
 		print(key + ": " + str(percent) + "% (" + str(value) + ") ")
 		file.write(key + ": " + str(percent) + "% (" + str(value) + ") \n")
 
+	# found winner
 	print("-----------------------------------")
 	file.write("-----------------------------------\n")
 	max_value = max(pollDict.values())
